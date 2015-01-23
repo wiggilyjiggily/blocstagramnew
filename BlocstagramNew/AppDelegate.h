e95b42230abb324fdf0075cece3d3fa4160e9157
Matthew Yang <imatthewyang@gmail.com>
2015-01-23 12:57:25 -0800

    Initial Commit

---
+++ BlocstagramNew/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  BlocstagramNew
+//
+//  Created by Matthew Yang on 1/23/15.
+//  Copyright (c) 2015 Tmrw, Inc. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
